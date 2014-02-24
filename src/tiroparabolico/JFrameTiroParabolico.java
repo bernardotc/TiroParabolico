e73ecc452c30f732bc4780f109617001b5f7a494
Bernardo Trevino <bernardotc94@gmail.com>
2014-02-24 14:52:13 -0600

    extends JFrame con KeyListener, MouseListener y MouseMotionListener

--- src/tiroparabolico/JFrameTiroParabolico.java
+++ src/tiroparabolico/JFrameTiroParabolico.java
@@ -3,13 +3,215 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package tiroparabolico;
+
+import javax.swing.JFrame;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.net.URL;
+import java.util.LinkedList;
 
 /**
  *
  * @author bernardot
  */
-public class JFrameTiroParabolico {
-    
+public class JFrameTiroParabolico extends JFrame implements Runnable, KeyListener, MouseListener, MouseMotionListener {
+
+    private Image dbImage;    // Imagen a proyectar
+    private Graphics dbg;
+
+    public void run() {
+
+        while (true) {
+            actualiza();
+            checaColision();
+
+            // Se actualiza el <code>Applet</code> repintando el contenido.
+            repaint();
+
+            try {
+                // El thread se duerme.
+                Thread.sleep(20);
+            } catch (InterruptedException ex) {
+                System.out.println("Error en " + ex.toString());
+            }
+        }
+    }
+
+    /**
+     * Metodo usado para actualizar la posicion de objetos heroe y ariados.
+     *
+     */
+    public void actualiza() {
+        //Determina el tiempo que ha transcurrido desde que el Applet inicio su ejecución
+
+    }
+
+    /**
+     * Metodo usado para checar las colisiones del objeto heroe y ariados con
+     * las orillas del <code>Applet</code>.
+     */
+    public void checaColision() {
+
+    }
+
+    /**
+     * Metodo <I>update</I> sobrescrito de la clase <code>Applet</code>,
+     * heredado de la clase Container.<P>
+     * En este metodo lo que hace es actualizar el contenedor
+     *
+     * @param g es el <code>objeto grafico</code> usado para dibujar.
+     */
+    public void paint(Graphics g) {
+        // Inicializan el DoubleBuffer
+        if (dbImage == null) {
+            dbImage = createImage(this.getSize().width, this.getSize().height);
+            dbg = dbImage.getGraphics();
+        }
+
+        // Actualiza la imagen de fondo.
+        dbg.setColor(getBackground());
+        dbg.fillRect(0, 0, this.getSize().width, this.getSize().height);
+
+        // Actualiza el Foreground.
+        dbg.setColor(getForeground());
+        paint1(dbg);
+
+        // Dibuja la imagen actualizada
+        g.drawImage(dbImage, 0, 0, this);
+    }
+
+    /**
+     * Metodo <I>mouseClicked</I> sobrescrito de la interface
+     * <code>MouseListener</code>.<P>
+     * En este metodo maneja el evento que se genera cuando se hace click en el
+     * ariados.
+     *
+     * @param e es el <code>evento</code> generado al presionar el ariados.
+     */
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    /**
+     * Metodo <I>mouseEnteredd</I> sobrescrito de la interface
+     * <code>MouseListener</code>.<P>
+     * En este metodo maneja el evento que se genera cuando el ariados entra.
+     *
+     *
+     * @param e es el <code>evento</code> generado al mover el ariados.
+     */
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    /**
+     * Metodo <I>mouseExited</I> sobrescrito de la interface
+     * <code>MouseListener</code>.<P>
+     * En este metodo maneja el evento que se genera cuando el ariados sale.
+     *
+     *
+     * @param e es el <code>evento</code> generado con el movimiento del
+     * ariados.
+     */
+    public void mouseExited(MouseEvent e) {
+    }
+
+    /**
+     * Metodo <I>mousePressed</I> sobrescrito de la interface
+     * <code>MouseListener</code>.<P>
+     * En este metodo maneja el evento que se genera al presionar el ariados.
+     *
+     *
+     * @param e es el <code>evento</code> generado al presionar el ariados.
+     */
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    /**
+     * Metodo <I>mouseReleased</I> sobrescrito de la interface
+     * <code>MouseListener</code>.<P>
+     * En este metodo maneja el evento que se genera al solta el ariados.
+     *
+     *
+     * @param e es el <code>evento</code> generado al soltar el ariados.
+     */
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    /**
+     * Metodo <I>mouseMoved</I> sobrescrito de la interface
+     * <code>MouseMotionListener</code>.<P>
+     * En este metodo maneja el evento que se genera al mover el ariados
+     *
+     *
+     * @param e es el <code>evento</code> generado al mover el ariados.
+     */
+    public void mouseMoved(MouseEvent e) {
+    }
+
+    /**
+     * Metodo <I>mouseDragged</I> sobrescrito de la interface
+     * <code>MouseMotionListener</code>.<P>
+     * En este metodo maneja el evento que se genera al mover el planeta una vez
+     * seleccionado.
+     *
+     * @param e es el <code>evento</code> generado al mover el planeta.
+     */
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    /**
+     * Metodo <I>keyPressed</I> sobrescrito de la interface
+     * <code>KeyListener</code>.<P>
+     * En este metodo maneja el evento que se genera al presionar cualquier la
+     * tecla.
+     *
+     * @param e es el <code>evento</code> generado al presionar las teclas.
+     */
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    /**
+     * Metodo <I>keyTyped</I> sobrescrito de la interface
+     * <code>KeyListener</code>.<P>
+     * En este metodo maneja el evento que se genera al presionar una tecla que
+     * no es de accion.
+     *
+     * @param e es el <code>evento</code> que se genera en al presionar las
+     * teclas.
+     */
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    /**
+     * Metodo <I>keyReleased</I> sobrescrito de la interface
+     * <code>KeyListener</code>.<P>
+     * En este metodo maneja el evento que se genera al soltar la tecla
+     * presionada.
+     *
+     * @param e es el <code>evento</code> que se genera en al soltar las teclas.
+     */
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    public void paint1(Graphics g) {
+        if (dbImage != null) {
+
+        } else {
+            //Da un mensaje mientras se carga el dibujo	
+            g.drawString("No se cargo la imagen..", 20, 20);
+        }
+    }
 }
