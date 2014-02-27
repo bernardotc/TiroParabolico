859135cf8c089c45129d2230f93be93f27161b76
Bernardo Trevino <bernardotc94@gmail.com>
2014-02-27 16:33:37 -0600

    Ya despliega instrucciones, guarda y carga. Falta: Diferentes velocidades, vidas, score.

--- src/tiroparabolico/JFrameTiroParabolico.java
+++ src/tiroparabolico/JFrameTiroParabolico.java
@@ -22,7 +22,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.FileWriter;
-import java.io.IOException; 
+import java.io.IOException;
 import java.util.Vector;
 
 /**
@@ -30,7 +30,7 @@ import java.util.Vector;
  * @author bernardot
  */
 public class JFrameTiroParabolico extends JFrame implements Runnable, KeyListener, MouseListener, MouseMotionListener {
-
+  private static final long serialVersionUID = 1L;
     private Caparazon caparazon; //objeto del caparazon
     private Tortuga tortuga; //objeto de la tortuga
 
@@ -40,34 +40,41 @@ public class JFrameTiroParabolico extends JFrame implements Runnable, KeyListene
     private boolean instrucciones;  // Valor booleano para mostrar/quitar instrucciones
     private boolean cargar;
     private boolean guardar;
-    private String instr; // String que contiene las instrucciones del juego.
+    private String instr1; // String que contiene las instrucciones del juego.
+    private String instr2; // String que contiene las instrucciones del juego.
+    private String instr3;
     private SoundClip shell;
     private SoundClip catched;
     private String nombreArchivo;   // Nombre del archivo
-    private Vector vec;     // Objeto vector
     private String[] arr;   // Arreglo del archivo dividido
-    
+
     // banderas
-        // de movimiento del caparazon
+    // de movimiento del caparazon
     private boolean click;
     private boolean volando;
-        //de movimiento de la tortuga
+    //de movimiento de la tortuga
     private boolean derecha;
     private boolean izquierda;
-    
+
     private boolean colision;
-    
 
     public JFrameTiroParabolico() {
         //Se inicializan variables
+        
         pausado = false;
         instrucciones = false;
         // TODO Corregir
-        instr = "El juego consiste en intentar atrapar la pelota con la canasta. Al momento de darle clic a la pelota, esta se moverá a través de la pantalla. Con las teclas izquierda y derecha, podrás mover la canasta. Si no llegas atrapar la pelota, la pelota caerá más rápido... ¡CUIDADO! ¡TU PUEDES!"; // Instrucciones del jugo
+        instr1 = "El juego consiste en intentar atrapar la pelota con la canasta. Al momento de darle clic a la pelota, esta se "; // Instrucciones del juego
+        instr2 = "moverá a través de la pantalla. Con las teclas izquierda y derecha, podrás mover la canasta. Si no llegas ";
+        instr3 = "atrapar la pelota, la pelota caerá más rápido... ¡CUIDADO! ¡TU PUEDES!";
         // END TODO Corregir
         // Se cargan los sonidos
         shell = new SoundClip("sounds/stomp.wav");
         catched = new SoundClip("sounds/marioSound.wav");
+
+        nombreArchivo = "Datos.txt";
+        cargar = false;
+        guardar = false;
         
         derecha = false;
         izquierda = false;
@@ -77,18 +84,17 @@ public class JFrameTiroParabolico extends JFrame implements Runnable, KeyListene
         setBackground(Color.white);
         setSize(800, 500);
 
-        tortuga = new Tortuga(getWidth()/2, (getHeight() - 50));
+        tortuga = new Tortuga(getWidth() / 2, (getHeight() - 50));
         caparazon = new Caparazon(5, getHeight() / 2);
-        
+
         addMouseListener(this);
         addMouseMotionListener(this);
         addKeyListener(this);
-        
-        
+
         // Declaras un hilo
         Thread th = new Thread(this);
         // Empieza el hilo
-        th.start();        
+        th.start();
     }
 
     public void run() {
@@ -129,50 +135,66 @@ public class JFrameTiroParabolico extends JFrame implements Runnable, KeyListene
                 caparazon.setPosX(caparazon.getPosX() + caparazon.getVelX());
                 caparazon.setPosY(caparazon.getPosY() + caparazon.getVelY());
             }
-            if (caparazon.getPosY() > getHeight()){
+            if (caparazon.getPosY() > getHeight()) {
                 volando = false;
-                caparazon.setPosY(getHeight()/2);
+                caparazon.setPosY(getHeight() / 2);
                 caparazon.setPosX(0);
                 caparazon.setVelX(0);
                 caparazon.setVelY(0);
             }
-            if (izquierda){
+            if (izquierda) {
                 tortuga.setVelX(tortuga.getVelX() - 2);
             }
-            if (derecha){
+            if (derecha) {
                 tortuga.setVelX(tortuga.getVelX() + 2);
             }
-            
+
             tortuga.setPosX(tortuga.getPosX() + tortuga.getVelX());
             tortuga.gravedad();
-            
-            if (tortuga.getPosX() < 0){
+
+            if (tortuga.getPosX() < 0) {
                 tortuga.setVelX(tortuga.getVelX() * -1);
             }
-            if (tortuga.getPosX() > getWidth() - tortuga.getAncho()){
+            if (tortuga.getPosX() > getWidth() - tortuga.getAncho()) {
                 tortuga.setVelX(tortuga.getVelX() * -1);
             }
-            
-            if (colision){
+
+            if (colision) {
                 caparazon.setPosX(0);
-                caparazon.setPosY(getHeight()/2);
+                caparazon.setPosY(getHeight() / 2);
                 caparazon.setVelX(0);
                 caparazon.setVelY(0);
                 volando = false;
                 colision = false;
             }
+            if (cargar) {
+                try {
+                   leeArchivo(); 
+                } catch (IOException e) {
+                    System.out.println("Error en " + e.toString());
+                }
+                cargar = false;
+            } 
             
-            
+            if (guardar) {
+                try {
+                   grabaArchivo(); 
+                } catch (IOException e) {
+                    System.out.println("Error en " + e.toString());
+                }
+                guardar = false;
+            } 
+
         }
 
     }
 
     /**
-     * Metodo usado para checar las colisiones del objeto tortuga y caparazon con
-     * entre si <code>Applet</code>.
+     * Metodo usado para checar las colisiones del objeto tortuga y caparazon
+     * con entre si <code>Applet</code>.
      */
     public void checaColision() {
-        if (tortuga.intersecta(caparazon)){
+        if (tortuga.intersecta(caparazon)) {
             colision = true;
         }
     }
@@ -318,7 +340,7 @@ public class JFrameTiroParabolico extends JFrame implements Runnable, KeyListene
         } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
             if (!derecha) {
                 derecha = true;
-            } 
+            }
         } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
             if (!izquierda) {
                 izquierda = true;
@@ -362,33 +384,51 @@ public class JFrameTiroParabolico extends JFrame implements Runnable, KeyListene
             }
         }
     }
-    
+
     /**
      * Metodo que lee a informacion de un archivo y lo agrega a un vector.
      *
      * @throws IOException
      */
-    public void leeArchivo() throws IOException{
-    	BufferedReader fileIn;
-    	try{
-    		fileIn = new BufferedReader(new FileReader(nombreArchivo));
-    	} catch (FileNotFoundException e){
-    		File puntos = new File(nombreArchivo);
-    		PrintWriter fileOut = new PrintWriter(puntos);
-    		fileOut.println("100,demo");
-    		fileOut.close();
-    		fileIn = new BufferedReader(new FileReader(nombreArchivo));
-    	}
-    	String dato = fileIn.readLine();
+    public void leeArchivo() throws IOException {
+        BufferedReader fileIn;
+        try {
+            fileIn = new BufferedReader(new FileReader(nombreArchivo));
+        } catch (FileNotFoundException e) {
+            File puntos = new File(nombreArchivo);
+            PrintWriter fileOut = new PrintWriter(puntos);
+            fileOut.println("5,250,0,0,false,false,250,450,0");
+            fileOut.close();
+            fileIn = new BufferedReader(new FileReader(nombreArchivo));
+        }
+        String dato = fileIn.readLine();
 
-    	while(dato != null) {
-    		arr = dato.split(",");
-    		int num = (Integer.parseInt(arr[0]));
-    		String nom = arr[1];
-    		//vec.add(new Puntaje(nom, num));
-    		dato = fileIn.readLine();
-    	}
-    	fileIn.close();
+        while (dato != null) {
+            arr = dato.split(",");
+            int cPosX = (Integer.parseInt(arr[0]));
+            int cPosY = (Integer.parseInt(arr[1]));
+            int cVelX = (Integer.parseInt(arr[2]));
+            int cVelY = (Integer.parseInt(arr[3]));
+            boolean c = (Boolean.parseBoolean(arr[4]));
+            boolean v = (Boolean.parseBoolean(arr[5]));
+            int tPosX = (Integer.parseInt(arr[6]));
+            int tPosY = (Integer.parseInt(arr[7]));
+            int tVelX = (Integer.parseInt(arr[8]));
+            caparazon.setPosX(cPosX);
+            caparazon.setPosY(cPosY);
+            caparazon.setVelX(cVelX);
+            caparazon.setVelY(cVelY);
+            click = c;
+            volando = v;
+            tortuga.setPosX(tPosX);
+            tortuga.setPosY(tPosY);
+            tortuga.setVelX(tVelX);
+            int num = (Integer.parseInt(arr[0]));
+            String nom = arr[1];
+            //vec.add(new Puntaje(nom, num));
+            dato = fileIn.readLine();
+        }
+        fileIn.close();
     }
 
     /**
@@ -396,26 +436,23 @@ public class JFrameTiroParabolico extends JFrame implements Runnable, KeyListene
      *
      * @throws IOException
      */
-    public void grabaArchivo() throws IOException{
-    	PrintWriter fileOut = new PrintWriter(new FileWriter(nombreArchivo));
-    	for (int i=0; i<vec.size(); i++) {
-    		//Puntaje x;
-    		//x = (Puntaje) vec.get(i);
-    		//fileOut.println(x.toString());
-    	}
-    	fileOut.close();	
+    public void grabaArchivo() throws IOException {
+        PrintWriter fileOut = new PrintWriter(new FileWriter(nombreArchivo));
+        fileOut.println("" + caparazon.getPosX() + "," + caparazon.getPosY() + "," + caparazon.getVelX() + "," + caparazon.getVelY() + "," + click + "," + volando + "," + tortuga.getPosX() + "," + tortuga.getPosY() + "," + tortuga.getVelX());
+        fileOut.close();
     }
 
     public void paint1(Graphics g) {
         if (caparazon != null && tortuga != null) {
-            g.drawString("velocidad Y:" + caparazon.getVelY(), getWidth()/2, 5);
-            g.drawString("velocidad X:" + caparazon.getVelX(), getWidth()/2, 15);
+            g.drawString("velocidad Y:" + caparazon.getVelY(), getWidth() / 2, 5);
+            g.drawString("velocidad X:" + caparazon.getVelX(), getWidth() / 2, 15);
             g.drawImage(caparazon.getImagenI(), caparazon.getPosX(), caparazon.getPosY(), this);
             g.drawImage(tortuga.getImagenI(), tortuga.getPosX(), tortuga.getPosY(), this);
             if (instrucciones) {
-                g.drawString(instr, 20, 20);
-            } else {
-
+                g.setColor(Color.BLACK);
+                g.drawString(instr1, 20, 80);
+                g.drawString(instr2, 20, 100);
+                g.drawString(instr3, 20, 120);
             }
         } else {
             //Da un mensaje mientras se carga el dibujo	
